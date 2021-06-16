8a5d128e928c59096a6a347c4012f5abe2609e1e
ridouchire <ridouchire@gmail.com>
2021-06-16 15:04:00 +0400

    Фикс для кроссдоменных запросов.

--- src/Application.php
+++ src/Application.php
@@ -26,6 +26,10 @@ class Application extends Container
         }
 
         header('Content-type: application/json');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+
         http_response_code($res->getCode());
         echo json_encode($res->getBody());
 
